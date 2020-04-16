3537e9b5d52007a3a8fd4384c49813852cc3ca0c
Yongnan Wu <yongnan@amazon.com>
2020-04-16 11:30:21 -0700

    9.1.1 fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { NgxMasonryModule } from 'ngx-masonry';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgxMasonryModule,
     AppRoutingModule
   ],
